f809dfbda72c34743813ce3b37963926deb75ab6
cmb69 <cmbecker69@gmx.de>
2014-06-28 11:57:40 +0000

    added Slovak translation (contributed by Tata)

---
+++ languages/sk.php
@@ -0,0 +1,12 @@
+<?php
+
+$plugin_tx['wdir']['label_name']="Názov súboru";
+$plugin_tx['wdir']['label_size']="Veľkosť";
+$plugin_tx['wdir']['label_modified']="Dátum zverejnenia";
+$plugin_tx['wdir']['label_file']="Súbor";
+$plugin_tx['wdir']['format_type']="%s file";
+$plugin_tx['wdir']['format_date']="m/d/Y h:i a";
+$plugin_tx['wdir']['alt_icon']="Zoznam súborov";
+$plugin_tx['wdir']['cf_filter_regexp']="Majú byť filtre používaé ako PERL kompatibilné štandardné výrazy (namiesto jednoduchých masiek). Vhodné iba pre skúsených webmastrov.";
+
+?>
