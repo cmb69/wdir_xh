50b1ca7adca37744e1d57385d2bc18e42dd420a0
cmb69 <cmbecker69@gmx.de>
2014-06-29 10:41:38 +0000

    fixed two language strings (contributed by Tata)

--- languages/sk.php
+++ languages/sk.php
@@ -4,8 +4,8 @@ $plugin_tx['wdir']['label_name']="Názov súboru";
 $plugin_tx['wdir']['label_size']="Veľkosť";
 $plugin_tx['wdir']['label_modified']="Dátum zverejnenia";
 $plugin_tx['wdir']['label_file']="Súbor";
-$plugin_tx['wdir']['format_type']="%s file";
-$plugin_tx['wdir']['format_date']="m/d/Y h:i a";
+$plugin_tx['wdir']['format_type']="Súbor %s";
+$plugin_tx['wdir']['format_date']="m.d.Y h:i a";
 $plugin_tx['wdir']['alt_icon']="Zoznam súborov";
 $plugin_tx['wdir']['cf_filter_regexp']="Majú byť filtre používaé ako PERL kompatibilné štandardné výrazy (namiesto jednoduchých masiek). Vhodné iba pre skúsených webmastrov.";
 
