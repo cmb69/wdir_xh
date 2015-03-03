1806289871282da2a6f825328fe8ff14a75b3042
Christoph M. Becker <cmbecker69@gmx.de>
2015-03-03 19:58:26 +0100

    made JS sorting compatible with older browsers (such as IE8)

--- wdir.js
+++ wdir.js
@@ -1,17 +1,64 @@
 /*jslint browser: true, maxlen: 80 */
 /*global addEventListener */
-if (typeof addEventListener === "function") {
-    addEventListener("load", function () {
-        "use strict";
+(function () {
+    "use strict";
 
+    function map(elements, func) {
+        var i, len, result;
+
+        result = [];
+        for (i = 0, len = elements.length; i < len; i += 1) {
+            result.push(func(elements[i], i));
+        }
+        return result;
+    }
+
+    function forEach(elements, func) {
+        var i, len;
+
+        for (i = 0, len = elements.length; i < len; i += 1) {
+            func(elements[i], i);
+        }
+    }
+
+    function on(element, type, listener) {
+        if (typeof element.addEventListener !== "undefined") {
+            element.addEventListener(type, listener, false);
+        } else if (typeof element.attachEvent !== "undefined") {
+            element.attachEvent("on" + type, listener);
+        }
+    }
+
+    on(window, "load", function () {
         var headings;
+
+
+        function findTableHeadingCells() {
+            var tables, result;
+
+            result = [];
+            tables = document.getElementsByTagName("table");
+            forEach(tables, function (table) {
+                if (table.className === "wdir_table") {
+                    var cells;
+
+                    cells = table.tHead.getElementsByTagName("td");
+                    forEach(cells, function (cell) {
+                        result.push(cell);
+                    });
+                }
+            });
+            return result;
+        }
 
         function sort(table, column, desc) {
             var tbody, rows;
 
-            tbody = table.querySelector("tbody");
-            rows = Array.prototype.map.call(tbody.rows, function (tr) {
-                var value = tr.getElementsByTagName("td")[column]
+            tbody = table.tBodies[0];
+            rows = map(tbody.rows, function (tr) {
+                var value;
+
+                value = tr.getElementsByTagName("td")[column]
                         .getAttribute("data-wdir");
                 if (column > 0) {
                     value = +value;
@@ -26,30 +73,30 @@ if (typeof addEventListener === "function") {
                     return (a || b) && !(a && b);
                 }
 
-                return a.value === b.value ? 0 : xor(a.value < b.value, desc) ?
-                        -1 : 1;
+                return a.value === b.value ? 0
+                        : xor(a.value < b.value, desc) ? -1 : 1;
             });
-            rows.forEach(function (value) {
+            forEach(rows, function (value) {
                 tbody.appendChild(value.element);
             });
         }
 
-        headings = document.querySelectorAll(".wdir_table thead tr td");
-        Array.prototype.forEach.call(headings, function (heading, index) {
+        headings = findTableHeadingCells();
+        forEach(headings, function (heading, index) {
             if (index % 3 === 0) {
                 heading.className = "wdir_asc";
             } else {
                 heading.className = "wdir_ascdesc";
             }
-            heading.addEventListener("click", function () {
+            on(heading, "click", function () {
                 var table, headings;
 
                 table = heading;
                 while (table.nodeName.toLowerCase() !== "table") {
                     table = table.parentNode;
                 }
-                headings = table.querySelectorAll("thead td");
-                Array.prototype.forEach.call(headings, function (heading2) {
+                headings = table.tHead.getElementsByTagName("td");
+                forEach(headings, function (heading2) {
                     if (heading2 !== heading) {
                         heading2.className = "wdir_ascdesc";
                     }
@@ -64,4 +111,4 @@ if (typeof addEventListener === "function") {
             });
         });
     });
-}
+}());
