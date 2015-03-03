712a245d14c6fa967e0360b530e26f3beafa30e1
Christoph M. Becker <cmbecker69@gmx.de>
2015-03-03 16:58:35 +0100

    added Russian translation (contributed by vasilev-lv)

---
+++ languages/ru.php
@@ -0,0 +1,12 @@
+<?php
+
+$plugin_tx['wdir']['label_name']="Имя";
+$plugin_tx['wdir']['label_size']="Размер";
+$plugin_tx['wdir']['label_modified']="Дата изменения";
+$plugin_tx['wdir']['label_file']="Файл";
+$plugin_tx['wdir']['format_type']="%s файл";
+$plugin_tx['wdir']['format_date']="m/d/Y h:i a";
+$plugin_tx['wdir']['alt_icon']="Посмотреть список файлов";
+$plugin_tx['wdir']['cf_filter_regexp']="Фильтры должны использовать Perl совместимое регулярное выражение (в отличие от простых моделей подстановки). Рекомендуем только для опытных веб-мастеров.";
+
+?>
