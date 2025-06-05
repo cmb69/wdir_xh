641df045f9b8a11ded36f244178bf3e36db8aab3
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-05 15:17:00 +0200

    Use GNU style file headers
    That PEAR non-sense makes no sense.

--- tests/FolderTest.php
+++ tests/FolderTest.php
@@ -1,18 +1,5 @@
 <?php
 
-/**
- * Testing the folder class.
- *
- * PHP version 5
- *
- * @category  Testing
- * @package   Wdir
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2012-2015 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Wdir_XH
- */
-
 namespace Wdir;
 
 use org\bovigo\vfs\vfsStreamWrapper;
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,5 @@
 <?php
 
-/**
- * The autoloader.
- *
- * PHP version 5
- *
- * @category  CMSimple_XH
- * @package   Testing
- * @author    Christoph M. Becker <cmbecker69@gmx.de>
- * @copyright 2015 Christoph M. Becker <http://3-magi.net>
- * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @link      http://3-magi.net/?CMSimple_XH/Wdir_XH
- */
-
 require_once "./vendor/autoload.php";
 
 require_once "../../cmsimple/functions.php";
