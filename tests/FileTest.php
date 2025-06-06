797e1d3458783750a28561d8f15e5de6ac5faa61
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-06 16:39:47 +0200

    Clean up model tests

--- tests/FileTest.php
+++ tests/FileTest.php
@@ -2,49 +2,50 @@
 
 namespace Wdir;
 
-use org\bovigo\vfs\vfsStreamWrapper;
-use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 class FileTest extends TestCase
 {
-    private File $subject;
-
     private string $path;
 
     protected function setUp(): void
     {
-        vfsStreamWrapper::register();
-        vfsStreamWrapper::setRoot(new vfsStreamDirectory('test'));
-        $this->path = vfsStream::url('test/foo.bar');
-        file_put_contents($this->path, 'foobar');
-        $this->subject = new File($this->path);
+        vfsStream::setup("test", null, [
+            "foo.bar" => "foobar",
+        ]);
+        $this->path = vfsStream::url("test/foo.bar");
+    }
+
+    private function sut(): File
+    {
+        return new File($this->path);
     }
 
     public function testPathIsCorrect(): void
     {
-        $this->assertEquals($this->path, $this->subject->getPath());
+        $this->assertEquals($this->path, $this->sut()->getPath());
     }
 
     public function testNameIsCorrect(): void
     {
-        $this->assertEquals('foo.bar', $this->subject->getName());
+        $this->assertEquals("foo.bar", $this->sut()->getName());
     }
 
     public function testExtensionIsCorrect(): void
     {
-        $this->assertEquals('bar', $this->subject->getExtension());
+        $this->assertEquals("bar", $this->sut()->getExtension());
     }
 
     public function testSizeIsCorrect(): void
     {
-        $this->assertEquals(6, $this->subject->getSize());
+        $this->assertEquals(6, $this->sut()->getSize());
     }
 
     public function testModificationTimeIsCorrect(): void
     {
-        touch($this->path, 123456);
-        $this->assertEquals(123456, $this->subject->getModificationTime());
+        $timestamp = strtotime("1970-01-02T10:17:36+00:00");
+        touch($this->path, $timestamp);
+        $this->assertEquals($timestamp, $this->sut()->getModificationTime());
     }
 }
--- tests/FolderTest.php
+++ tests/FolderTest.php
@@ -2,33 +2,32 @@
 
 namespace Wdir;
 
-use org\bovigo\vfs\vfsStreamWrapper;
-use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
 
 class FolderTest extends TestCase
 {
+    private string $filter;
     private array $conf;
 
     protected function setUp(): void
     {
-        vfsStreamWrapper::register();
-        vfsStreamWrapper::setRoot(new vfsStreamDirectory('test'));
-        file_put_contents(vfsStream::url('test/foo.txt'), '***');
-        touch(vfsStream::url('test/foo.txt'), 345678);
-        file_put_contents(vfsStream::url('test/bar.txt'), '**');
-        touch(vfsStream::url('test/bar.txt'), 234567);
-        file_put_contents(vfsStream::url('test/Baz.txt'), '*');
-        touch(vfsStream::url('test/Baz.txt'), 123456);
-        touch(vfsStream::url('test/foo.bar'));
-
+        vfsStream::setup("test", null, [
+            "foo.txt" => "***",
+            "bar.txt" => "**",
+            "Baz.txt" => "*",
+            "foo.bar" => "",
+        ]);
+        touch(vfsStream::url('test/foo.txt'), strtotime("1970-01-05T00:01:18+00:00"));
+        touch(vfsStream::url('test/bar.txt'), strtotime("1970-01-03T17:09:27+00:00"));
+        touch(vfsStream::url('test/Baz.txt'), strtotime("1970-01-02T10:17:36+00:00"));
+        $this->filter = "*.txt";
         $this->conf = XH_includeVar("./config/config.php", "plugin_cf")["wdir"];
     }
 
     private function sut(): Folder
     {
-        return new Folder(vfsStream::url('test/'), '*.txt', "en", $this->conf);
+        return new Folder(vfsStream::url('test/'), $this->filter, "en", $this->conf);
     }
 
     public function testTwoFilesAreFound(): void
@@ -72,17 +71,17 @@ class FolderTest extends TestCase
 
     public function testSimpleFilter(): void
     {
+        $this->filter = "?a?.txt";
         $this->conf["filter_regexp"] = "";
-        $subject = new Folder(vfsStream::url('test/'), '?a?.txt', "en", $this->conf);
-        $this->assertCount(2, $subject->getFiles());
-        $subject = new Folder(vfsStream::url('test/'), 'foo.*', "en", $this->conf);
-        $this->assertCount(2, $subject->getFiles());
+        $this->assertCount(2, $this->sut()->getFiles());
+        $this->filter = "foo.*";
+        $this->assertCount(2, $this->sut()->getFiles());
     }
 
     public function testRegexpFilter(): void
     {
+        $this->filter = '/^foo/';
         $this->conf["filter_regexp"] = "true";
-        $subject = new Folder(vfsStream::url('test/'), '/^foo/', "en", $this->conf);
-        $this->assertCount(2, $subject->getFiles());
+        $this->assertCount(2, $this->sut()->getFiles());
     }
 }
